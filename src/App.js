50240823b54fcfe016254f0a5d3c6c51f8133853
Free Wifi <quickerz59@gmail.com>
2023-01-01 19:08:19 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "@mui/material";
+import InputForm from "./components/InputForm";
+
+function App() {
+  return (
+    <div className="App">
+      <InputForm/>
+    </div>
+  );
+}
+
+export default App;
