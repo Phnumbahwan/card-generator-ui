30d27608555be7f125ae7b1ba2446ab92e6d51db
Gil lopez Pacturan <gil.pacturan@fligno.com>
2023-01-04 15:41:27 +0800

    update

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const customInstance = axios.create ({
-  baseURL : process.env.REACT_APP_BACKEND_URL + '/api/',
+  baseURL : '/api/',
   headers: {'Accept': 'application/json'}
 })
 
